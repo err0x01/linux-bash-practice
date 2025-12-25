2a7602c32dfe85dadfdfce953a35891f04395f71
Ilya Delitsyn <henry-curie@yandex.ru>
2025-12-25 19:50:41 +0300

    linux practice script

---
+++ task.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir -p task/dir1 task/dir2 task/dir3/dir4
+
+cd task
+
+touch dir2/empty
+
+echo '#!/bin/bash
+echo "$1, привет!"' > dir2/hello.sh
+
+chmod 764 dir2/hello.sh
+
+ls dir2 > dir2/list.txt
+
+cp -r dir2/. dir3/dir4/
+
+find . -name "*.txt" > dir1/summary.txt
+
+cat dir2/list.txt >> dir1/summary.txt
+
+NAME="Всем студентам"
+
+./dir2/hello.sh "$NAME" >> dir1/summary.txt
+
+mv dir1/summary.txt "Практическое задание"
+
+cat "Практическое задание"
+
+grep "dir" "Практическое задание" | sort
+
+cd ..
+
+rm -rf task
